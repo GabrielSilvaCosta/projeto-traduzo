580b740dac71ec5dac430753f0ce1eafd82e45fa
GabrielSilvaCosta <bob@>
2023-12-07 11:55:27 -0300

    feat: requisito 2 feito

---
+++ src/models/language_model.py
@@ -0,0 +1,10 @@
+# src/models/language_model.py
+from src.models.abstract_model import AbstractModel
+from database.db import db
+
+
+class LanguageModel(AbstractModel):
+    _collection = db["languages"]
+
+    def __init__(self, data):
+        super().__init__(data)
